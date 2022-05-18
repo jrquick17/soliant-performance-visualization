7707f558bcc766bb6b34a304e73db93b003af361
Jeremy Quick <jeremy.quick@bullhorn.com>
2022-05-18 13:17:11 -0500

    A little spring cleanin'

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,84 +8,15 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private excludedFields = [
+  private excludedFields:string[] = [
     'count(id)'
   ];
 
   constructor(
     private http:HttpClient,
-    private ngxCsvParser: NgxCsvParser
+    private ngxCsvParser:NgxCsvParser
   ) {
 
-  }
-
-  private getParamValues(key:string, query:string, split:string = ',') {
-    let results:any[] = [];
-
-    const startingIndex = query.indexOf(key);
-    if (startingIndex >= 0) {
-      let fields = query.slice(startingIndex + key.length);
-
-      const lastIndex = fields.indexOf('&');
-      if (lastIndex >= 0) {
-        fields = fields.slice(0, lastIndex);
-      }
-
-      fields = fields.replace('-', '')
-        .replace('+', '')
-        .replace('\'', '');
-
-      results = results.concat(fields.split(split));
-    }
-
-    return results;
-  }
-
-  private getFieldNames(query:string) {
-    return this.getParamValues('fields=', query);
-  }
-
-  private getWhereNames(query:string) {
-    const results:any[] = this.getParamValues('where=', query, ' AND ');
-
-    results.forEach(
-      (result) => {
-        result = result.splice(result.indexOf('<'));
-        result = result.splice(result.indexOf('>'));
-        result = result.splice(result.indexOf('!'));
-        result = result.splice(result.indexOf('='));
-      }
-    );
-
-    return results;
-  }
-
-  private getShowName(query:string) {
-    return this.getParamValues('showTotalMatched=', query);
-  }
-
-  private getOrderByName(query:string) {
-    return this.getParamValues('orderBy=', query);
-  }
-
-  private getGroupByName(query:string) {
-    return this.getParamValues('groupBy=', query);
-  }
-
-  private getUniqueCallID(query:string) {
-    return this.getParamValues('uniqueCallId=', query);
-  }
-
-  private getCountName(query:string) {
-    return this.getParamValues('count=', query);
-  }
-
-  private isExcludedField(field:string):boolean {
-    return this.excludedFields.some(
-      (blacklistedField) => {
-        return blacklistedField.toLowerCase() === field.toLowerCase();
-      }
-    );
   }
 
   public get(type:string):Observable<any[]> {
@@ -102,37 +33,16 @@ export class DataService {
           const csv = this.ngxCsvParser.csvStringToArray(file, ',');
           csv.forEach(
             (row, index) => {
-              row[3] = decodeURIComponent(row[3]);
+              const query = decodeURIComponent(row[3]);
 
-              if (index !== 0 && typeof row[4] === 'string' && typeof row[3] === 'string') {
-                let names:any[] = [];
-                switch (type) {
-                  case 'count':
-                    names = this.getCountName(row[3]);
-                    break;
-                  case 'fields':
-                    names = this.getFieldNames(row[3]);
-                    break;
-                  case 'groupBy':
-                    names = this.getGroupByName(row[3]);
-                    break;
-                  case 'orderBy':
-                    names = this.getOrderByName(row[3]);
-                    break;
-                  case 'showTotalMatched':
-                    names = this.getShowName(row[3]);
-                    break;
-                  case 'where':
-                    names = this.getWhereNames(row[3]);
-                    break;
-                }
-
+              if (index !== 0 && typeof row[4] === 'string') {
+                let names = DataService._getNames(type, query);
                 const value = parseInt(row[4].replace(',', ''));
 
                 if (names.length !== 0) {
                   names.forEach(
-                    (name) => {
-                      if (!this.isExcludedField(name)) {
+                    (name:string) => {
+                      if (!this._isExcludedField(name)) {
                         let entry = results.find(
                           (result: any) => {
                             return result.name === name;
@@ -142,7 +52,7 @@ export class DataService {
                         if (!entry) {
                           entry = {
                             name: name,
-                            query: row[3],
+                            query: query,
                             count: 0,
                             total: 0,
                             value: 0,
@@ -159,7 +69,7 @@ export class DataService {
 
                         entry.data.push(file);
 
-                        const uniqueIDs: string[] = this.getUniqueCallID(row[3]);
+                        const uniqueIDs: string[] = DataService.getUniqueCallID(query);
                         if (uniqueIDs.length !== 0) {
                           entry.uniqueIDs.push(uniqueIDs[0]);
                         }
@@ -188,6 +98,103 @@ export class DataService {
           return results;
         }
       )
+    );
+  }
+
+  private static getCountName(query:string) {
+    return DataService._getParamValues('count=', query);
+  }
+
+  private static _getFieldNames(query:string) {
+    return DataService._getParamValues('fields=', query);
+  }
+
+  private static getGroupByName(query:string) {
+    return DataService._getParamValues('groupBy=', query);
+  }
+
+  private static _getNames(type: string, query: string):string[] {
+    let names:string[] = [];
+    switch (type) {
+      case 'count':
+        names = DataService.getCountName(query);
+        break;
+      case 'fields':
+        names = DataService._getFieldNames(query);
+        break;
+      case 'groupBy':
+        names = DataService.getGroupByName(query);
+        break;
+      case 'orderBy':
+        names = DataService.getOrderByName(query);
+        break;
+      case 'showTotalMatched':
+        names = DataService.getShowName(query);
+        break;
+      case 'where':
+        names = DataService._getWhereNames(query);
+        break;
+    }
+
+    return names;
+  }
+
+  private static getOrderByName(query:string) {
+    return DataService._getParamValues('orderBy=', query);
+  }
+
+  private static _getParamValues(key:string, query:string, split:string = ',') {
+    let results:any[] = [];
+
+    const startingIndex = query.indexOf(key);
+    if (startingIndex >= 0) {
+      let fields = query.slice(startingIndex + key.length);
+
+      const lastIndex = fields.indexOf('&');
+      if (lastIndex >= 0) {
+        fields = fields.slice(0, lastIndex);
+      }
+
+      fields = fields.replace('-', '')
+        .replace('+', '')
+        .replace('\'', '');
+
+      results = results.concat(fields.split(split));
+    }
+
+    return results;
+  }
+
+  private static getShowName(query:string) {
+    return DataService._getParamValues('showTotalMatched=', query);
+  }
+
+  private static getUniqueCallID(query:string) {
+    return DataService._getParamValues('uniqueCallId=', query);
+  }
+
+  private static _getWhereNames(query:string) {
+    const results:any[] = DataService._getParamValues('where=', query, ' AND ');
+
+    results.forEach(
+      (result) => {
+        result = result.splice(result.indexOf('<'));
+        result = result.splice(result.indexOf('>'));
+        result = result.splice(result.indexOf('!'));
+        result = result.splice(result.indexOf('='));
+
+        return result;
+      }
+    );
+
+    return results;
+  }
+
+  private _isExcludedField(field:string):boolean {
+    return this.excludedFields.some(
+      (blacklistedField) => {
+        return blacklistedField.toLowerCase() === field.toLowerCase();
+      }
     );
   }
 }
